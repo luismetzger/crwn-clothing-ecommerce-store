029cfd2387b156e255a1445fe4fd1a0f8f99f07f
Luis Metzger <luismetzger@me.com>
2021-07-02 11:47:47 -0600

    Add new components for collection items and preview.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import { Route } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import { Homepage } from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 import './App.css';
 
 function App() {
   return (
     <div>
-        <Route exact path='/' component={ Homepage } />
+        <Switch>
+            <Route exact path='/' component={ Homepage } />
+            <Route exact path='/shop' component={ ShopPage } />
+        </Switch>
     </div>
   );
 }
